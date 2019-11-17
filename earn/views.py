ffd0f51415e7288e19a7a25994a60648ea6194a3
Anshul Pratap <anshulpratap821@gmail.com>
2019-11-17 22:06:03 +0530

    version 2

--- earn/views.py
+++ earn/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from random import choice
 
 def index(request):
@@ -13,26 +12,49 @@ def show(request):
         , 'Prabha', 'Pinki', 'Shweta']
 
     complement = ['arrey ye to tatti h', 'Handsome boy', 'Number 1 in flirting',
-                  'Dharti pe Boj', 'kya H ye Naam enter karvaya h kya Enter kar rha h "tatti"',
+                  'Dharti pe Boj', 'ittu sa h iska',
                   'Smarty londe', 'choribaaz', 'Dieheart Gabru', 'Bihaari hai sala', 'katai zeher',
                   'Chugalkhor', 'auntiyon pe buri nazar daalna band kar de', 'saale kutega tu to',
                   'andi bandi sandi aage samaj jaa', 'sala Feku', 'Hot man', 'maccho man', 'chal chakke'
         , 'Oe Aashiq', 'bhaag Bhutni ke', 'muh mein lele', 'bhai setting karva de ki rat lagana vala',
                   'Hurrrr bhaag bc']
 
+    if request.GET.get("text") == ("Hemant"):
+        gf = "Neha"
+        comp = choice(complement)
+        params = {'msg': Name, 'complement': comp, 'gf': gf}
+        return render(request, 'show.html', params)
 
-    gf = choice(gfs)
-    comp = choice(complement)
-    params = {'msg': Name, 'complement': comp, 'gf': gf}
-    return render(request, 'show.html', params)
+    elif  request.GET.get("text") == ("Abhisek"):
+        gf = "Chhavi"
+        comp = choice(complement)
+        params = {'msg': Name, 'complement': comp, 'gf': gf}
+        return render(request, 'show.html', params)
+
+    elif  request.GET.get("text") == ("Golu"):
+        gf = "varsha"
+        comp = choice(complement)
+        params = {'msg': Name, 'complement': comp, 'gf': gf}
+        return render(request, 'show.html', params)
+
+    elif  request.GET.get("text") == ("Kapil"):
+        gf = "B***C"
+        comp = "saale ye Name allowed nhi h dusra try kar BC"
+        params = {'msg': Name, 'complement': comp, 'gf': gf}
+        return render(request, 'show.html', params)
+
+    elif  request.GET.get("text") == ("Anshul"):
+        gf = choice(gfs)
+        compl = "Smarty boy, All girls have crush on him"
+        params = {'msg': Name, 'complement': compl, 'gf': gf}
+        return render(request, 'show.html', params)
 
 
-
-
-
-
-
-
+    else:
+        gf = choice(gfs)
+        comp = choice(complement)
+        params = {'msg': Name, 'complement': comp, 'gf': gf}
+        return render(request, 'show.html', params)
 
 
 
